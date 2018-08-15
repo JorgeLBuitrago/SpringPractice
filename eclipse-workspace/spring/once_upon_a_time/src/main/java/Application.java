bae1de601bbf3c5a35c3a5b7f4dfb5b452dc0156
jorgebuitrago@ymail.com <jorgebuitrago@ymail.com>
2018-08-15 12:21:34 -0400

    [King Removed from Application.java to Quest.java]

--- eclipse-workspace/spring/once_upon_a_time/src/main/java/Application.java
+++ eclipse-workspace/spring/once_upon_a_time/src/main/java/Application.java
@@ -11,8 +11,6 @@ import com.jorge.model.Quest;
 import com.jorge.service.EmbarkService;
 
 public class Application {
-    private static String king;
-
 
 	public static void main(String[] args) {
 
@@ -27,12 +25,12 @@ public class Application {
 		Iterator<Quest> itQuest = questList.iterator();
 
 		while (itHero.hasNext() && itQuest.hasNext()) {
-			Quest quest= itQuest.next();
+			Quest quest = itQuest.next();
 			Hero hero = itHero.next();
-			System.out.println("King " + quest.getKing()  +" has arrived. All rise!");
+			System.out.println("King " + quest.getKing() + " has arrived. All rise!");
 			System.out.println("King declares that " + hero.getFullName());
 			System.out.println("Will embark on journey to " + quest.getDescription());
-			if(!"".equals(hero.getWeapon())) {
+			if (!"".equals(hero.getWeapon())) {
 				System.out.println("He will be using his favorite weapon ... " + hero.getWeapon());
 				System.out.println("against ... " + quest.getEnemy() + "");
 				System.out.println("");
@@ -42,15 +40,5 @@ public class Application {
 		((ConfigurableApplicationContext) ac).close();
 
 	}
-	
-	public static String getKing() {
-		return king;
-	}
 
-	@Value("${king}")
-	public static void setKing(String king) {
-		Application.king = king;
-	}
-
-	
 }
